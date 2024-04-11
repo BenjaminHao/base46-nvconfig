1b8219819438c37cb57ed2d3c0536266600100d3
Mingkai Hao <haomingkai@gmail.com>
2024-04-11 15:00:56 -0700

    chore: modify preview

--- lua/nvconfig/lua_symbols_preview.lua
+++ lua/nvconfig/lua_symbols_preview.lua
@@ -1,21 +1,19 @@
---╭──────────────────────────────────────────────────────────────────────────╮--
---│                  This file is for theme preview purpose.                 │--
---╰──────────────────────────────────────────────────────────────────────────╯--
-local my_variable = "Hello, world!" -- Strings
-local number = 42 -- numbers
-local result = (number + 10) * 2 -- Operators
-local my_table = {apple = "fruit", car = "vehicle", dog = "animal"} -- Tables
+-- This file is for demonstrating themes.
+-- This is how comments look like.
+local my_variable = "Hello, world!"
+local number = 42
+local result = (number + 10) * 2
+local my_table = {apple = "fruit", car = "vehicle", dog = "animal"}
 
-local function greet(name) -- Functions
+local function greet(name)
     print("Hello, " .. name)
 end
 
--- Control Structures
-if number > 0 then
+if number > 0 then -- if bigger than 0
     print(number .. " is positive.")
-elseif number == 0 then
+elseif number == 0 then -- if equal to 0
     print("Number is 0")
-else
+else -- if less than 0
     print(number .. " is negative.")
 end
 
@@ -23,14 +21,12 @@ for i = 1, 5 do
     print("Iteration " .. i)
 end
 
--- Metatables (for custom behavior)
 local mt = {}
 mt.__index = function(_, key)
     return "Unknown: " .. key
 end
 setmetatable(my_table, mt)
 
--- Print the symbols
 print(my_variable)
 greet("Lua")
 print(result)
